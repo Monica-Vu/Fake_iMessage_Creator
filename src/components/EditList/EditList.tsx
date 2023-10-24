8efc521510c447c389a7a277a5d46b664af9f93b
Monica <vu.monica.05@gmail.com>
2023-10-24 11:06:00 -0700

    Change hard-coded data to messages

--- src/components/EditList/EditList.tsx
+++ src/components/EditList/EditList.tsx
@@ -1,52 +1,48 @@
 import React, { useState } from "react";
 import { DragDropContext, Droppable, Draggable, type OnDragEndResponder } from 'react-beautiful-dnd';
 import MessagesContext, { MessagesType } from "../../context/MessageContext/MessageContext";
+import ImageCrop from "../ProfilePictureUpload/ImageCrop";
+import Button from "../Common/Button/Button";
 
-const initialTasks = [
-    {
-        id: "1",
-        title: "Task 1",
-    },
-    {
-        id: "2",
-        title: "Task 2",
-    },
-    {
-        id: "3",
-        title: "Task 3",
-    },
-];
+const handleUpdateList = () => {
+    console.log("handleUpdateList is called")
+}
 
 const EditList: React.FC = ({ }) => {
-    const [tempList, setTempList] = useState(initialTasks);
+    const { messages, setMessages } = React.useContext(MessagesContext) as MessagesType;
 
     //@ts-expect-error
     const onDragEnd = (result) => {
         if (!result.destination) return;
 
-        const items = Array.from(tempList);
+        const items = Array.from(messages || []);
         const [reorderedItem] = items.splice(result.source.index, 1);
         items.splice(result.destination.index, 0, reorderedItem);
 
-        setTempList(items);
+        setMessages(messages);
     }
 
     return (
+        <div> 
         <DragDropContext onDragEnd={onDragEnd}>
-            <Droppable droppableId="tasks">
+            <Droppable droppableId="messages">
                 {(provided) => (
                     <div ref={provided.innerRef} {...provided.droppableProps}>
-                        {tempList.map((task, index) => (
+                        {messages?.map((message, index) => (
                             <Draggable 
-                                key={task.id}
-                                draggableId={task.id}
+                                key={message.id}
+                                draggableId={message.id}
                                 index={index}> 
                                 {(provided) => (
                                     <div 
                                     {...provided.draggableProps}
                                     {...provided.dragHandleProps}
                                     ref={provided.innerRef}>
-                                    <div>{task.title}</div>
+                                    <div>
+                                        {message.text}
+                                        {message.image && <ImageCrop file={message.image} width="20%" height="20%" />}
+                                        {message.date}
+                                    </div>
                                     </div>
                                 )}
                                 </Draggable>
@@ -55,6 +51,8 @@ const EditList: React.FC = ({ }) => {
                     </div>)}
             </Droppable>
         </DragDropContext >
+        <Button text="Update" onClick={handleUpdateList}/>
+        </div>
     )
 }
 
