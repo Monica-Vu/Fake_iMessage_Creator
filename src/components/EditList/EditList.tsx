f42183232292f854d266df432bd6232dd5a1e26e
Monica <vu.monica.05@gmail.com>
2023-10-24 11:56:43 -0700

    Added delete functionality

--- src/components/EditList/EditList.tsx
+++ src/components/EditList/EditList.tsx
@@ -3,6 +3,7 @@ import { DragDropContext, Droppable, Draggable, type OnDragEndResponder } from '
 import MessagesContext, { MessagesType, type Message } from "../../context/MessageContext/MessageContext";
 import ImageCrop from "../ProfilePictureUpload/ImageCrop";
 import Button from "../Common/Button/Button";
+import "./EditList.css"
 
 const EditList: React.FC = ({ }) => {
     const { messages, setMessages, setIsEditing } = React.useContext(MessagesContext) as MessagesType;
@@ -12,7 +13,11 @@ const EditList: React.FC = ({ }) => {
         setMessages(tempMessages);
         setIsEditing(false);
     }
-    
+
+    const handleCancelEditList = () => {
+        setIsEditing(false);
+    }
+
     //@ts-expect-error
     const onDragEnd = (result) => {
         if (!result.destination) return;
@@ -24,36 +29,43 @@ const EditList: React.FC = ({ }) => {
         setTempMessages(items);
     }
 
+    const handleDelete = (id: string) => {
+        const newList = tempMessages?.filter((item) => item.id !== id);
+        setTempMessages(newList || [])
+    }
+
     return (
-        <div> 
-        <DragDropContext onDragEnd={onDragEnd}>
-            <Droppable droppableId="messages">
-                {(provided) => (
-                    <div ref={provided.innerRef} {...provided.droppableProps}>
-                        {tempMessages?.map((message, index) => (
-                            <Draggable 
-                                key={message.id}
-                                draggableId={message.id}
-                                index={index}> 
-                                {(provided) => (
-                                    <div 
-                                    {...provided.draggableProps}
-                                    {...provided.dragHandleProps}
-                                    ref={provided.innerRef}>
-                                    <div>
-                                        {message.text}
-                                        {message.image && <ImageCrop file={message.image} width="20%" height="20%" />}
-                                        {message.date}
-                                    </div>
-                                    </div>
-                                )}
+        <div>
+            <DragDropContext onDragEnd={onDragEnd}>
+                <Droppable droppableId="messages">
+                    {(provided) => (
+                        <div ref={provided.innerRef} {...provided.droppableProps}>
+                            {tempMessages?.map((message, index) => (
+                                <Draggable
+                                    key={message.id}
+                                    draggableId={message.id}
+                                    index={index}>
+                                    {(provided) => (
+                                        <div
+                                            {...provided.draggableProps}
+                                            {...provided.dragHandleProps}
+                                            ref={provided.innerRef}>
+                                            <div className="list-item">
+                                                {message.text}
+                                                {message.image && <ImageCrop file={message.image} width="20%" height="20%" />}
+                                                {message.date}
+                                                <Button text="delete" onClick={() => handleDelete(message.id)} colour="red" />
+                                            </div>
+                                        </div>
+                                    )}
                                 </Draggable>
-                        ))}
-                        {provided.placeholder}
-                    </div>)}
-            </Droppable>
-        </DragDropContext >
-        <Button text="Update" onClick={handleUpdateList}/>
+                            ))}
+                            {provided.placeholder}
+                        </div>)}
+                </Droppable>
+            </DragDropContext >
+            <Button text="Update" onClick={handleUpdateList} />
+            <Button text="Cancel" onClick={handleCancelEditList} />
         </div>
     )
 }
