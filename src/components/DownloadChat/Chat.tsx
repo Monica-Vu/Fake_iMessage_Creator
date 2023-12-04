e1c0a945e4db1caa4042aa0ae2af63f32a8933a8
Monica <vu.monica.05@gmail.com>
2023-12-03 22:09:56 -0800

    Don't remove speech bubble tail if it's a different person messaging

--- src/components/DownloadChat/Chat.tsx
+++ src/components/DownloadChat/Chat.tsx
@@ -88,19 +88,27 @@ const Chat: React.FC<ChatProps> = ({ chatRef }) => {
 
       <div id="chat">
         {messages?.map((bubble, index) => {
+          const sameContactId = messages[index + 1].contactId === messages[index].contactId
+
           const removeTail = () => {
             if (index <= messages.length - 2) {
-              if (
-                messages[index].hasOwnProperty("text") &&
-                messages[index + 1].hasOwnProperty("text") &&
-                messages[index].sender === messages[index + 1].sender
+              if (messages[index].hasOwnProperty("text") &&
+                messages[index + 1].hasOwnProperty("text")
               ) {
-                return true;
+                if (messages[index].sender === messages[index + 1].sender) {
+                  if (messages[index].sender) {
+                    return true
+                  } else {
+                    if (sameContactId) {
+                      return true
+                    } else {
+                      return false
+                    }
+                  }
+                }
               }
             }
           };
-
-          // TODO: add logic to check if the contact name is the same as the previous, remove the profile picture 
 
           const contactIdExists = bubble.contactId != null
 
